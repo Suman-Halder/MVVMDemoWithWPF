d5b863fdbf47340af661d0e7e39e61cba7b77e09
Suman Halder <sumanhalder.rcciit@gmail.com>
2020-04-11 00:39:42 +0530

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> cart = new List<int>() { 0,1, 0, 3, 1, 3 };
+            List<int> ucart = new List<int>();
+            for (int i = 0; i < cart.Count(); i++)
+            {
+                if (cart.Count() >= 1 && ucart.Count() < 1)
+                {
+                    ucart.Add(cart[0]);
+                    //updatedCartItems[0].Quantity = 0;
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    if(ucart[j] == cart[i])
+                    {
+
+                    }
+                    else if(!IsContain(ucart, cart[i]))
+                    {
+                        ucart.Add(cart[i]);
+                    }
+                }
+
+            }
+             
+        }
+
+        private static bool IsContain(List<int> Items, int Item)
+        {
+            bool isPresent = false;
+            foreach(int item in Items)
+            {
+                if(item == Item)
+                {
+                    isPresent = true;
+                    break;
+                }
+            }
+            return isPresent;
+        }
+    }
+}
---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public IEnumerable Colours_P { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //cmbColors.ItemsSource = Colours;
+            //this.DataContext = this;
+            //this.GroupBoxes = new ObservableCollection<GroupBox>();
+            //this.GroupBoxes.Add(new GroupBox() { Name = "AAA", Header = "AAA", Height = 100, Background = Brushes.Purple });
+            //this.GroupBoxes.Add(new GroupBox() { Name = "BBB", Header = "BBB", Height = 100, Background = Brushes.Purple });
+            //this.GroupBoxes.Add(new GroupBox() { Name = "CCC", Header = "CCC", Height = 100, Background = Brushes.Purple });
+            //this.GroupBoxes.Add(new GroupBox() { Name = "DDD", Header = "DDD", Height = 100, Background = Brushes.Purple });
+            //this.GroupBoxes.Add(new GroupBox() { Name = "EEE", Header = "EEE", Height = 100, Background = Brushes.Purple });
+        }
+        public class Colour
+        {
+            public string Name { get; set; }
+        }
+
+        public List<Colour> Colours
+        {
+            get
+            {
+                return GetColors();
+            }
+            set
+            {
+
+            }
+
+        }
+
+        public static List<Colour> GetColors()
+        {
+            List<Colour> colours = new List<Colour>();
+            colours.Add(new Colour() { Name = "Red" });
+            colours.Add(new Colour() { Name = "Blue" });
+            colours.Add(new Colour() { Name = "Green" });
+            colours.Add(new Colour() { Name = "Black" });
+            colours.Add(new Colour() { Name = "Navy" });
+            return colours;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //var item = cmbColors.SelectedValue;
+        }
+    }
+}
+
+
+
+//#region GroupBoxesProperty
+
+//public static readonly DependencyProperty GroupBoxesProperty = DependencyProperty.Register(
+//    "GroupBoxes", typeof(ObservableCollection<GroupBox>), typeof(MainWindow)
+//);
+
+//public ObservableCollection<GroupBox> GroupBoxes
+//{
+//    get { return (ObservableCollection<GroupBox>)GetValue(GroupBoxesProperty); }
+//    set { SetValue(GroupBoxesProperty, value); }
+//}
+
+//#endregion
+
+//#region SelectedGroupBoxProperty
+
+//public static readonly DependencyProperty SelectedGroupBoxProperty = DependencyProperty.Register(
+//    "SelectedGroupBox", typeof(GroupBox), typeof(MainWindow),
+//    new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, (s, e) => (s as MainWindow).OnSelectedGroupBoxChanged())
+//);
+
+//public GroupBox SelectedGroupBox
+//{
+//    get { return (GroupBox)GetValue(SelectedGroupBoxProperty); }
+//    set { SetValue(SelectedGroupBoxProperty, value); }
+//}
+
+//void OnSelectedGroupBoxChanged()
+//{
+//    Console.WriteLine("selection is now " + this.SelectedGroupBox.Name);
+//}
+
+//#endregion
+
+
+
+
