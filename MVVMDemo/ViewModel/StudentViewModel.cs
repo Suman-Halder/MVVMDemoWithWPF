4cb9b355c4dbe33fa153b3b6db3451436a88dba5
Suman Halder <sumanhalder.rcciit@gmail.com>
2020-04-11 21:43:49 +0530

    After Implementing ICommend for Delete Button

--- MVVMDemo/ViewModel/StudentViewModel.cs
+++ MVVMDemo/ViewModel/StudentViewModel.cs
@@ -5,14 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MVVMDemo.Model;
+using MVVMDemo.Misc;
 
 namespace MVVMDemo.ViewModel
 {
     public class StudentViewModel
     {
         public ObservableCollection<Student> Students { get; set; }
+        public MyICommand DeleteCommand { get; set; }
 
         public StudentViewModel()
+        {
+            LoadStudent();
+            DeleteCommand = new MyICommand(OnDelete, CanDelete);
+        }
+
+        void LoadStudent()
         {
             ObservableCollection<Student> students = new ObservableCollection<Student>();
             students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
@@ -20,6 +28,31 @@ namespace MVVMDemo.ViewModel
             students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
 
             Students = students;
+        }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get
+            {
+                return _selectedStudent;
+            }
+            set
+            {
+                _selectedStudent = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void OnDelete()
+        {
+            Students.Remove(SelectedStudent);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedStudent != null;
         }
     }
 }
